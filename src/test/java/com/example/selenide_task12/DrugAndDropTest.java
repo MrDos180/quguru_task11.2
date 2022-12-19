3b318722f09c1b7ae9fff85a26447dea1d4fad15
MrDos180 <ilya180@yandex.ru>
2022-12-19 20:07:13 +0300

    add drug and drop actions test

--- src/test/java/com/example/selenide_task12/DrugAndDropTest.java
+++ src/test/java/com/example/selenide_task12/DrugAndDropTest.java
@@ -1,10 +1,11 @@
 package com.example.selenide_task12;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class DrugAndDropTest extends TestBase{
@@ -14,6 +15,20 @@ public class DrugAndDropTest extends TestBase{
             open("https://the-internet.herokuapp.com/drag_and_drop");});
         step("Перетаскиваем квадрат А на квадрат Б", () ->{
             $("#column-a").dragAndDropTo($("#column-b"));});
+        step("Проверяем что квадраты поменялись местами", () ->{
+            $("#column-a header").shouldHave(Condition.text("B"));
+            $("#column-b header").shouldHave(Condition.text("A"));});
+    }
+    @Disabled
+    @Test
+    void drugAndDropActionsTest(){
+        step("Открывыаем страницу", () ->{
+            open("https://the-internet.herokuapp.com/drag_and_drop");});
+        step("Перетаскиваем квадрат А на квадрат Б", () ->{
+            actions().
+                    clickAndHold($("#column-a")).
+                    moveToElement($("#column-b")).
+                    release().build().perform();});
         step("Проверяем что квадраты поменялись местами", () ->{
             $("#column-a header").shouldHave(Condition.text("B"));
             $("#column-b header").shouldHave(Condition.text("A"));});
